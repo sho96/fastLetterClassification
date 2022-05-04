db3208aa7c99c8d02e780fd4c718895e931e9e3b
sho96 <81459356+sho96@users.noreply.github.com>
2022-05-04 17:57:07 +0900

    Update imgClassifier.py

--- imgClassifier.py
+++ imgClassifier.py
@@ -4,9 +4,12 @@ import numpy as np
 import random
  
 class Classifier:
-    def __init__(self, imgSize, outputs):
+    def __init__(self, imgSize, outputs, randomWeights = True, initVal = 1):
         self.imgSize = imgSize
-        self.weights = np.array([[[random.uniform(-1.0, 1.0)] * imgSize[0] for i in range(imgSize[1])] for i in range(outputs)], dtype = np.float32)
+        if randomWeights:
+            self.weights = np.array([[[random.uniform(-1.0, 1.0)] * imgSize[0] for i in range(imgSize[1])] for i in range(outputs)], dtype = np.float32)
+        else:
+            self.weights = np.array([[[initVal] * imgSize[0] for i in range(imgSize[1])] for i in range(outputs)], dtype = np.float32)
         self.outputs = np.array([0] * outputs, dtype = np.float32)
         self.biases = np.array([0] * outputs , dtype = np.float32)
         
