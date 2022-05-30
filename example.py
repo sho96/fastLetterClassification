f706c12d2c12de23741f8a70ca68ada1895b71e5
sho96 <81459356+sho96@users.noreply.github.com>
2022-05-30 21:49:01 +0900

    Update example.py

--- example.py
+++ example.py
@@ -3,13 +3,13 @@ import numpy as np
 import pickle
 from keras.datasets import mnist
 
-#create a model
+#create a model with an input shape of 28x28 and 10 outputs
 model = imgClassifier.Classifier((28, 28), 10)
 
 #load mnist data
 (trainX, trainY),(testX, testY) = mnist.load_data()
 
-#train a model
+#train a model with input and its label (index of an output neuron)
 for img, label in zip(trainX, trainY):
     model.train(img, label)
 
@@ -24,14 +24,13 @@ for img, label in zip(testX, testY):
 accuracy = corrects / len(testX)
 print(f"accuracy: {accuracy * 100}%")
 
-#you can use pickle library to save your model and use it again on another projects (this works on the same project, of course)
+#you can use pickle library to save your model and use it again on another projects
 data = pickle.dumps(model)
-f = open("~file path~", "wb")
-f.write(data)
-f.flush()
-f.close()
+with open("~file path~", "rb") as f:
+    f.write(data)
 
-#loading model from a file
-f = open("~file path~", "rb")
-nodel = pickle.loads(f.read())
-f.close()
+#loading model from a file and rebuilding an model
+with open("~file path~", "rb") as f:
+    model = pickle.dumps(f.read())
+print(model)
+
